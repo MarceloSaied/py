888fcd649aea2c7758032a3d772e406210323186
Marcelo Saied <marcelosaied@gmail.com>
2016-09-22 12:13:19 -0300

    cont.

--- python/General/general.py
+++ python/General/general.py
@@ -44,3 +44,38 @@ for var in list(range(5)):
 	print (var)
 
 #------------------------------------
+for letter in 'Python':     # traversal of a string sequence
+   print ('Current Letter :', letter)
+print()
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:        # traversal of List sequence
+   print ('Current fruit :', fruit)
+
+print ("Good bye!")
+
+#-------------------------
+fruits = ['banana', 'apple',  'mango']
+for index in range(len(fruits)):
+   print ('Current fruit :', fruits[index])
+
+print ("Good bye!")
+
+#--------------------------------
+numbers=[11,33,55,39,55,75,37,21,23,41,13]
+
+for num in numbers:
+    if num%2==0:
+        print ('the list contains an even number')
+        break
+else:
+    print ('the list doesnot contain even number')
+
+#----------------------------
+
+
+
+
+
+
+
+
